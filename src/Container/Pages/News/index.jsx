a7f24a459dfc15d04de1e34573793a128e4791ac
Andy Chow <hhchow7-c@my.cityu.edu.hk>
2021-07-18 19:50:57 +0800

    rough UI for News

--- src/Container/Pages/News/index.jsx
+++ src/Container/Pages/News/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
+import CollapseTable from '../../../Component/CollapseTable'
+
 function News() {
+    const data = [
+        {id: 1, date: '07-07-2021', title: "topic 1", content: "content 1",},
+        {id: 2, date: '08-07-2021', title: "topic 2", content: "content 2"}
+    ];
+
     return (
-        <div>
-            News
-        </div>
+        <div className="section-wrapper">
+            <div className="text-left">
+                <span className="section-header">News</span>
+            </div>
+            <div>           
+                <CollapseTable data={data}/>
+            </div>
+        </div>  
     )
 }
 
